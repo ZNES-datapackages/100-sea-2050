a46496b5d38d4f127d4815f60ce46e75c3e2dc94
simnh <simon.hilpert@uni-flensburg.de>
2018-09-14 11:19:52 +0200

    Fix meta data script

--- scripts/infer_metadata.py
+++ scripts/infer_metadata.py
@@ -1,9 +1,5 @@
 
-from datapackage_utilities import building
-
-building.infer_metadata(keep_resources=False)
-
 
 if __name__ == "__main__":
     from datapackage_utilities import building
-    building.add_metadata(keep_resources=True)
+    building.infer_metadata(keep_resources=True)
